ffb095750bc03f545388a05f09f39fa109c606ac
Ryo Manzoku <manzoku.ryo@gmail.com>
2018-10-22 08:55:27 +0000

    Graph def

--- lib/loom.go
+++ lib/loom.go
@@ -20,7 +20,44 @@ func (p LoomPlugin) FetchMetrics() (map[string]float64, error) {
 
 // GraphDefinition interface for mackerelplugin
 func (p LoomPlugin) GraphDefinition() map[string]mp.Graphs {
-	return nil
+	return map[string]mp.Graphs{
+		"block": {
+			Label: "Block count",
+			Unit:  "integer",
+			Metrics: []mp.Metrics{
+				{Name: "count", Label: "Count", Diff: false},
+			},
+		},
+		"evm.gas": {
+			Label: "EVM Gas cost",
+			Unit:  "integer",
+			Metrics: []mp.Metrics{
+				{Name: "cost", Label: "Gas cost", Diff: false},
+			},
+		},
+		"request": {
+			Label: "Loomchain request count",
+			Unit:  "integer",
+			Metrics: []mp.Metrics{
+				{Name: "application", Label: "Application", Diff: false},
+				{Name: "query", Label: "Query", Diff: false},
+				{Name: "tx", Label: "Transaction", Diff: false},
+				{Name: "evm_tx", Label: "EVM transaction", Diff: false},
+			},
+		},
+		"latency": {
+			Label: "Loomchain latency (microseconds)",
+			Unit:  "integer",
+			Metrics: []mp.Metrics{
+				{Name: "check_tx", Label: "Check tx", Diff: false},
+				{Name: "commit_block", Label: "Commit block", Diff: false},
+				{Name: "deliver_tx", Label: "Deliver tx", Diff: false},
+				{Name: "evm_tx", Label: "EVM tx", Diff: false},
+				{Name: "query", Label: "Query request", Diff: false},
+				{Name: "tx", Label: "Tx request", Diff: false},
+			},
+		},
+	}
 }
 
 // MetricKeyPrefix interface for PluginWithPrefix
@@ -31,7 +68,7 @@ func (p LoomPlugin) MetricKeyPrefix() string {
 // Do do doo
 func Do() {
 	var (
-		optPrefix   = flag.String("metric-key-prefix", "", "Metric key prefix")
+		optPrefix   = flag.String("metric-key-prefix", "loomchain", "Metric key prefix")
 		optURL      = flag.String("url", "http://127.0.0.1:46658/metrics", "Loom RPC Bind Address")
 		optTempfile = flag.String("tempfile", "", "Temp file name")
 	)
